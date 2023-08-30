fd8e138d53b252a257db7aff9fe2520b5d50487a
Kameleon21 <kamilrogozinski29@gmail.com>
2023-08-30 09:25:00 +0100

    changed layout of web

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// target values of the form
+const form = document.querySelector(".form");
+const firstName = document.querySelector(".name");
+const email = document.querySelector(".email");
+const country = document.querySelector(".country");
+const zipCode = document.querySelector(".zipcode");
+const pass = document.querySelector(".password");
+const confirmPassword = document.querySelector(".conPassword");
 
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailErro;
+  } else {
+  }
+});
